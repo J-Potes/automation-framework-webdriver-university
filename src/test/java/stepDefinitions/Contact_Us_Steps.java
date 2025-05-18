c88a1a7d6d5a98f035a66c0645fc6f883dd867d5
J-Potes <jpotes.multimedia@gmail.com>
2025-05-18 18:11:43 -0500

    added first Given step definition method on Contact_Us_Steps.java

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,4 +1,11 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+
 public class Contact_Us_Steps {
+
+    @Given("I access the webdriver university contact us page")
+    public void i_access_the_webdriver_university_contact_us_page(){
+
+    }
 }
