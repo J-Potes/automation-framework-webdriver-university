2eb314cbb00e89c0db5f2e01a7336455e6062965
Juan Jose Potes Gomez <juan.potes@globant.com>
2025-05-23 18:09:26 -0500

    Added steps definitions for successful submission scenario on Contact_Us_Steps

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -59,6 +59,24 @@ public class Contact_Us_Steps {
     public void i_enter_a_unique_comment() {
         driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("Hello world " + generateRandomString(20));
     }
+
+    @When("I enter a specific first name {word}")
+    public void i_enter_a_specific_first_name(String firstName) {
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstName);
+    }
+    @And("I enter a specific last name {word}")
+    public void i_enter_a_specific_last_name(String lastName) {
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys(lastName);
+    }
+    @And("I enter a specific email address {word}")
+    public void i_enter_a_specific_email_address(String email) {
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+    }
+    @And("I enter a specific comment {string}")
+    public void i_enter_a_specific_comment(String comment) {
+        driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys(comment);
+    }
+
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
         driver.findElement(By.xpath("//input[@type='submit']")).click();
@@ -69,5 +87,3 @@ public class Contact_Us_Steps {
         Assert.assertEquals(contactUs_Submission_Message.getText(), "Thank You for your Message!");
     }
 }
-
-//This is a test 2
