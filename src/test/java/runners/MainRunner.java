46aa575b3e439d3d78703a0fa164e3c66ebb89a9
Juan Jose Potes Gomez <juan.potes@globant.com>
2025-06-17 11:15:18 -0500

    Added MainRunner.java code

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -1,4 +1,10 @@
 package runners;
 
-public class MainRunner {
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
+        tags = "@login", monochrome = false, dryRun = false,
+        plugin = {"pretty", "html:target/cucumber"})
+public class MainRunner extends AbstractTestNGCucumberTests{
 }
