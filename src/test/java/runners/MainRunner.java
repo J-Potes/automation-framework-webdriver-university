9ba61663b4b0b3d006d1774cff243e9dea0308b4
Juan Jose Potes Gomez <juan.potes@globant.com>
2025-06-18 10:36:10 -0500

    Added code for parallel execution on MainRunner.java file

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -2,9 +2,15 @@ package runners;
 
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
         tags = "@login", monochrome = false, dryRun = false,
         plugin = {"pretty", "html:target/cucumber"})
 public class MainRunner extends AbstractTestNGCucumberTests{
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
