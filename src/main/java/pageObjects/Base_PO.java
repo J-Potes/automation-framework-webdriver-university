4d7739d4da7657dd0e2fc6144d3c94cc209d10b1
Juan Jose Potes Gomez <juan.potes@globant.com>
2025-06-19 12:32:04 -0500

    Added another sendKeys method on Base_PO.java file but receiving a WebElement as parameter

--- src/main/java/pageObjects/Base_PO.java
+++ src/main/java/pageObjects/Base_PO.java
@@ -4,6 +4,7 @@ import driver.DriverFactory;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,5 +32,10 @@ public class Base_PO {
     public void sendKeys(By by, String textToType){
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(by)).sendKeys(textToType);
+    }
+
+    public void sendKeys(WebElement element, String textToType){
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).sendKeys(textToType);
     }
 }
