e70f166eb95a78ba72b7f2bdbcc408e9a9f4aa97
Juan Jose Potes Gomez <jj.potesgomez@gmail.com>
2025-07-25 08:27:23 -0500

    Added attributes and methods for username, password and login_button on Login_PO.java

--- src/main/java/pageObjects/Login_PO.java
+++ src/main/java/pageObjects/Login_PO.java
@@ -1,7 +1,36 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Login_PO extends Base_PO{
+    private @FindBy(id = "text")
+    WebElement username_TextField;
+
+    private @FindBy(id = "password")
+    WebElement password_TextField;
+
+    private @FindBy(id = "login-button")
+    WebElement login_Button;
+
     public Login_PO(){
         super();
+    }
+
+    public Login_PO(WebDriver driver){
+        super(driver);
+    }
+
+    public void setUsername(String username){
+        sendKeys(username_TextField, username);
+    }
+
+    public void setPassword(String password){
+        sendKeys(password_TextField, password);
+    }
+
+    public void clickOnLoginButton(){
+        clickElement(login_Button);
     }
 }
