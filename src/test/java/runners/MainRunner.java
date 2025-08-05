ae8497e262d97c0f954290dda778127acb05d925
Juan Jose Potes Gomez <jj.potesgomez@gmail.com>
2025-08-04 21:41:20 -0500

    Deleted tag in MainRunner.java so that it can be sent from Jenkins

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@login", monochrome = false, dryRun = false,
+        tags = "", monochrome = false, dryRun = false,
         plugin = {"pretty", "html:target/cucumber"})
 public class MainRunner extends AbstractTestNGCucumberTests{
     @Override
